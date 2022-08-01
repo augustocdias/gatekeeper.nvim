03998514f9cdefc62a3755a876cceba10dcfd15d
Augusto <augusto.c.dias@wire.com>
2022-08-01 08:57:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Gatekeeper.nvim
+
+This plugin intends to make plugins outside of the current working directory
+read only and non modifiable. To do that it sets an auto command for every
+time a buffer gets loaded.
+
+The purpose for the existence of this plugin is basically allowing to set
+white lists for folders or files.
+
+## Usage
+
+Install with your favorite package manager and call
+
+```lua
+require('gatekeeper').setup({
+    exclude = {}
+})
+```
+
+By default the exclude is an empty list. You can add folders or files to it.
+It won't expand `~` to your home so you have to concatenate `vim.fn.expand('~')`
+to your desired folder, if that's the case. Example:
+
+```lua
+require('gatekeeper').setup({
+    exclude = {
+        vim.fn.expand('~/.config/nvim')
+    }
+})
+```
+
+You can call `:GTForceWrite` on any buffer to revert and force it to be writable
+and modifiable.
---
+++ lua/gatekeeper.lua
@@ -0,0 +1,40 @@
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = function(name)
+    return vim.api.nvim_create_augroup(name, { clear = true })
+end
+local M = {}
+
+local options = {
+    exclude = {},
+}
+
+local function is_excluded(bufname, cwd)
+    for _, val in pairs(options.exclude) do
+        if string.sub(bufname, 1, string.len(val)) == val then
+            return true
+        end
+    end
+    return string.sub(bufname, 1, string.len(cwd)) == cwd
+end
+
+function M.setup(opts)
+    options = vim.tbl_deep_extend('force', options, opts or {})
+    local group = augroup('Gatekeeper.nvim')
+    autocmd({ 'BufReadPost' }, {
+        desc = 'Make files readonly when outside of current working dir',
+        pattern = '*',
+        group = group,
+        callback = function()
+            if not is_excluded(vim.api.nvim_buf_get_name(0), vim.fn.getcwd()) then
+                vim.bo.readonly = true
+                vim.bo.modifiable = false
+            end
+        end,
+    })
+    vim.api.nvim_create_user_command('GTForceWrite', function()
+        vim.bo.readonly = false
+        vim.bo.modifiable = true
+    end, { desc = 'Sets current buffer to be writable and modifiable' })
+end
+
+return M
